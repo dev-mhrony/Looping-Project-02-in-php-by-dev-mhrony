00757330dfe91e13172af6f358e10821db86feb8
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-10 17:55:05 +0600

    10/04/2022

---
+++ Assignment 04/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    //=================================== Qurstien 04 ===============================================
+
+// Let’s traverse this array(0,10,80,67,60,89,91,56,45,30,95,83,99) and print 0 index then skip 1 index again print 2 skip 3
+
+    $number = array(0,10,80,67,60,89,91,56,45,30,95,83,99);
+
+   foreach($number )
+
+
+
+
+
+
+?>
