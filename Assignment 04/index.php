f83c10d3af3525ace63b2d9ff2c7843e2917ed07
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-10 23:48:31 +0600

    Last Update - 10/04/2022

--- Assignment 04/index.php
+++ Assignment 04/index.php
@@ -6,7 +6,47 @@
 
     $number = array(0,10,80,67,60,89,91,56,45,30,95,83,99);
 
-   foreach($number )
+   foreach($number as $index => $singlen_number){            //Here this condition is used to rotate the above values through a loop.
+      
+    if ($index == 0){                                       // Here you have to skip the 0nd index for which the condition is created by the value of 1rd or 0nd index ==.
+            
+            continue;                                       //A variable called Continue is taken to skip the value of this cell which is a building function of PHP.
+
+       }elseif ($index == 2){                               // Here you have to skip the 2nd index for which the condition is created by the value of 3rd or 2nd index ==.
+            
+            continue;                                       //A variable called Continue is taken to skip the value of this cell which is a building function of PHP.
+       
+        }elseif ($index == 4){                               // Here you have to skip the 4th index for which the condition is created by the value of 5rd or 4nd index ==.
+            
+            continue;                                       //A variable called Continue is taken to skip the value of this cell which is a building function of PHP.
+       
+        }elseif ($index == 6){                               // Here you have to skip the 6th index for which the condition is created by the value of 7rd or 6nd index ==.
+            
+            continue;                                       //A variable called Continue is taken to skip the value of this cell which is a building function of PHP.
+       
+        }elseif ($index == 8){                               // Here you have to skip the 8th index for which the condition is created by the value of 9rd or 8nd index ==.
+            
+            continue;                                       //A variable called Continue is taken to skip the value of this cell which is a building function of PHP.
+       
+        }elseif ($index == 10){                               // Here you have to skip the 10th index for which the condition is created by the value of 11rd or 10nd index ==.
+            
+            continue;                                       //A variable called Continue is taken to skip the value of this cell which is a building function of PHP.
+           
+        };
+
+
+       echo $singlen_number;                                // The remaining number that will be printed will be through it.
+
+       echo "<br>";                                         // It is used to give a break between each value.
+
+
+
+};
+
+
+
+   
+
 
 
 
