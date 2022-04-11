a40df75ac8793d646705022d1ab09b70d78ce6e5
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-11 10:43:40 +0600

    Max Number Find in Array - 11/04/2022

--- Assignment 03/index.php
+++ Assignment 03/index.php
@@ -13,23 +13,26 @@
 
 
 
-    $number = array(0,10,80,67,60,89,91,56,45,30,95,83,99);
+    $number = array(0,10,80,67,60,89,91,56,45,30,95,83,99);                     //Here all the data has been taken through array.
 
-    $largnumber = 0 ;
+    $largnumber = 0 ;                                                           //Here the value of 0 index is considered to be the largest number with maximum.
 
-    foreach ($number as $SingleNumber){
-        $largnumber = $largnumber + 1;
+    foreach ($number as $SingleNumber){                                         // Here it is used to find out the single value through forech.
+
+        $largnumber = $largnumber + 1;                                          //It is used to determine the value in a single way according to the value of the index.
     }
 
-    $maxNumber = $number[0];
+    $maxNumber = $number[0];                                                    // Here the 0 value is taken as the highest value.
 
-    for ($i = 1 ; $i < $largnumber ; $i++){
-        if ($number [$i] > $maxNumber ){
-            $maxNumber = $number [$i];
+    for ($i = 1 ; $i < $largnumber ; $i++){                                     // Here the highest quality index array is rotated.
+       
+        if ($number [$i] > $maxNumber ){                                        // Here a larger value is determined by comparing the conditions.
+
+            $maxNumber = $number [$i];                                          // Here the highest value is determined.
         }
     }
 
-    echo "The number <b> $maxNumber </b> is the maximum value of this array." ;
+    echo "The number <b> $maxNumber </b> is the maximum value of this array." ;     //Max Number Print 
 
 
 
