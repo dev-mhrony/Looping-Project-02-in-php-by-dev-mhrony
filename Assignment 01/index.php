4ee22051e4e709f9ef719abfe790be89d18b06da
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-04-11 21:04:16 +0600

    Last Update - 11/04/2022

---
+++ Assignment 01/index.php
@@ -0,0 +1,57 @@
+<?php
+
+    /*
+        Question 01: 
+
+            We have an array [12,34,2,6,78]. What kind of array is this? Find all prime numbers from this array.
+
+            Step 1: First let us find the factors of the given number( factors are the number that completely divides the given number)
+
+            Step 2: Then check the total number of factors of that number
+
+            Step 3: Hence, If the total number of factors is more than two, it is not a prime number but a composite number. Because When a number is divisible by only one and itself, then it is a prime number.
+    */
+
+
+
+    // $number = [12,34,2,6,78];
+
+    // foreach ($number as $index => $SingleValue){
+        
+    //     $snumber = $SingleValue ;
+    //     $Larg = 0;
+    
+    // }
+    
+    // for($i = 1; $i < $snumber; $i++){
+    
+    //     if ($snumber % $i == 0 ){
+    
+    //         $Larg++;
+    
+    //         break ;
+    //     }
+    // } if($Larg = 0){
+    //     echo  "$snumber This is a Prime Number ";
+        
+    // }else {
+    //     echo "$snumber This in a Not Prime Number";
+    // };
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
